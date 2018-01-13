d066ffc3c64a4b4eff553e6728cdd45a3082a451
Muhammad Salah <ziko_950211@yahoo.com>
2018-01-14 00:18:41 +0200

    init

---
+++ MyApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Product1= new Product{ID=1,Name="Surface",Price=300};
+            
+            var jsonString = JsonConvert.SerializeObject(Product1);
+
+            Console.WriteLine(jsonString);
+
+            var Product2 = JsonConvert.DeserializeObject<Product>(jsonString);
+
+            Console.WriteLine($"Name: {Product2.Name}");
+            Console.WriteLine($"ID: {Product2.ID}");
+            Console.WriteLine($"Price: {Product2.Price}");
+        }
+    }
+
+    class Product
+    {
+        public int ID{get;set;}
+        public string Name{get;set;}
+        public double Price{get;set;}
+    }
+}
